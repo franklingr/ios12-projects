8bf23494fa468305c6411f9a6cbf730443442a10
Franklin Grijalva Revelo <franklingr2009@hotmail.com>
2019-05-20 04:01:26 +0200

    Initial Commit

---
+++ 01-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Franklin Grijalva Revelo on 20/05/2019.
+//  Copyright © 2019 Franklin Grijalva Revelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
