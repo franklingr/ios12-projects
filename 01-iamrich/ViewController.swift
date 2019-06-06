12f73a656d6a1e98ec00ad063b354146d680f878
Franklin Grijalva Revelo <franklingr2009@hotmail.com>
2019-06-06 16:27:47 +0200

    Proyecto 1 finalizado

--- 01-iamrich/ViewController.swift
+++ 01-iamrich/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //PROPERTIES
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var imageViewDiamond: UIImageView!
+    @IBOutlet weak var buttonPush: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    //METHODS
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let controller = UIAlertController(title: "I am Rich", message: """
+                                                    I am rich,
+                                                    I deserve it,
+                                                    I am good,
+                                                    healty and successfull
+                                                    """, preferredStyle: .actionSheet)
+        
+        let action = UIAlertAction(title: "Aceptar", style: .default) { (action) in
+            print("He pulsado el botón aceptar")
+        }
+        
+        controller.addAction(action)
+        
+        let action2 = UIAlertAction(title: "Borrar", style: .destructive, handler: { (action) in
+            print("He pulsado el botón Borrar")
+        })
+        
+        controller.addAction(action2)
+        
+        let action3 = UIAlertAction(title: "Cancelar", style: .cancel) { _ in
+            print("He pulsado el botón Cancelar")
+        }
+        
+        controller.addAction(action3)
+        
+        
+        
+        self.show(controller, sender: nil)
+        
+        
+    }
+    
 }
 
